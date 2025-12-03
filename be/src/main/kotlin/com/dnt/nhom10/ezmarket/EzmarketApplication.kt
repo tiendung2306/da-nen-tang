842405a54fc2a032d58edbf5d025fa761887fa92
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2025-12-04 01:14:10 +0700

    Initialize Spring Boot project with PostgreSQL configuration and Gradle setup

---
+++ be/src/main/kotlin/com/dnt/nhom10/ezmarket/EzmarketApplication.kt
@@ -0,0 +1,11 @@
+package com.dnt.nhom10.ezmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EzmarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<EzmarketApplication>(*args)
+}
---
+++ be/src/test/kotlin/com/dnt/nhom10/ezmarket/EzmarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dnt.nhom10.ezmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EzmarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
